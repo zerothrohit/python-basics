58b92c2933d9f4e0fda91e9f570a0bcd44d0d3a5
Rohit Wahwal <80590966+zerothwrath@users.noreply.github.com>
2021-03-24 00:10:24 +0530

    Program for temperature converiosn
    This pro

---
+++ Temp_Conversion_Assignment_2.py
@@ -0,0 +1,6 @@
+#Rohit Wahwal(Mumbai) Dhruva Group
+
+print('---Program for Temperature conversion from Fahrenheit to Centigrade---')
+fah=int(input('Enter the value of temperature in Degree Fahrenheit: '))
+c=((fah-32)*(5))/9
+print('The value of Given Temperature in Degree Centigrade',c)
